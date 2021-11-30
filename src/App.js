7057c7d86a6b9d0c91ef8f60787eae1fa0868fcf
Iftakhar Husan <iftakharn19@gmail.com>
2021-11-30 21:50:47 +0600

    Render CardList component on component mount

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
 // import logo from './logo.svg';
-import './App.css';
-
-
+import "./App.css";
 
 export default class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
-    return (
-      <div>
-        {this.state.monsters.map(monster => <h1 key={monster.id}>{monster.id} - {monster.name}</h1>)}
-      </div>
-    )
+    return <CardList monsters={this.state.monsters} />;
   }
 }
